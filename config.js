18666c681c4987830b344939159f6a55d68d1a89
Harsh Shah <harshvinayshah@gmail.com>
2018-06-12 10:56:47 -0400

    initial bot ready

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: 'mERnh3xDduUm1mIYdMblpuu9t',
+    consumer_secret: 'IaS8Nj74MoYUqSFf805TDyfRr40YLCebHHWqHiX6Ieigp10IAX',
+    access_token_key: '600301213-zMqMn3w3UQec9PYW892OAXSTjRL3hQcd2om3a8mu',
+    access_token_secret: 'xVuAb9KOUwbg9TQKtGNJ79nCW8J7SeCXsRXFj0LA0Xfue'
+  }
